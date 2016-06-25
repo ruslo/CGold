4953ae4fe9581e147e3867b55db58cfd72a0c1d6
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-06-26 01:36:18 +0300

    Update year

--- docs/conf.py
+++ docs/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'CGold'
-copyright = u'2015 Ruslan Baratov'
+copyright = u'2015-2016, Ruslan Baratov'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
