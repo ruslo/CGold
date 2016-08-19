3394e94560a9076880fb357b0096dbc14a208fef
Ruslan Baratov <ruslan_baratov@yahoo.com>
2016-08-19 23:41:21 +0300

    Exclude '_venv'

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ release = '0.1'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'rtfd-css', 'examples/docs.rst']
+exclude_patterns = ['_build', '_venv', 'rtfd-css', 'examples/docs.rst']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
